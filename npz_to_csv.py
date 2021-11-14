a17b14886a04080fd3b6af8b0cb3fb816d316181
Tadej Vobner <t.vobner@gmail.com>
2021-11-14 16:26:27 +0100

    First AI model

---
+++ npz_to_csv.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pickle
+
+with open("D:\EEG Raziskovalna\Data\EEG_DE_features_csv\Participants_test.csv", "a") as f:
+    data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(16))
+    data = pickle.loads(data_npz['data'])
+    labels = pickle.loads(data_npz['label'])
+    for key, value in data.items():
+
+        arr = np.array(labels[key])
+        arr_reshaped = np.reshape(arr,(len(arr),1))
+ 
+        solution = np.concatenate((value,arr_reshaped), axis=1)
+        np.savetxt(f, solution)
+
+        
+
+
+
+        
