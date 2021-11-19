762008ffa18b7420c5b94939bba86b3cc3d6728c
Luka <luka.jeza@gmail.com>
2021-11-19 17:40:45 +0100

    added comments to npz to csv

--- npz_to_csv.py
+++ npz_to_csv.py
@@ -8,9 +8,11 @@ for i in range(1,17):
         data = pickle.loads(data_npz['data'])
         labels = pickle.loads(data_npz['label'])
         for key, value in data.items():
-
+            # first creating a vector (value) wich cointains all features data
+            # then creating a another vector(arr) wich cointains the labels
             arr = np.array(labels[key])
+            # reshaping arr into a vertical vector
             arr_reshaped = np.reshape(arr,(len(arr),1))
-    
+            # merging the two vectors together
             solution = np.concatenate((value,arr_reshaped), axis=1)
             np.savetxt(f, solution)
