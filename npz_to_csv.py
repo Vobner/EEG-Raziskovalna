c0c5d223ab5585ac3e6581cd706eff727752a396
Luka <luka.jeza@gmail.com>
2021-11-19 17:37:04 +0100

    novi npz to csv

--- npz_to_csv.py
+++ npz_to_csv.py
@@ -1,20 +1,16 @@
 import numpy as np
 import pickle
 
-with open("D:\EEG Raziskovalna\Data\EEG_DE_features_csv\Participants_test.csv", "a") as f:
-    data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(16))
-    data = pickle.loads(data_npz['data'])
-    labels = pickle.loads(data_npz['label'])
-    for key, value in data.items():
 
-        arr = np.array(labels[key])
-        arr_reshaped = np.reshape(arr,(len(arr),1))
- 
-        solution = np.concatenate((value,arr_reshaped), axis=1)
-        np.savetxt(f, solution)
+for i in range(1,17):
+    with open("csvData/participant{}".format(i) + ".csv", "a") as f:
+        data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(i))
+        data = pickle.loads(data_npz['data'])
+        labels = pickle.loads(data_npz['label'])
+        for key, value in data.items():
 
-        
-
-
-
-        
+            arr = np.array(labels[key])
+            arr_reshaped = np.reshape(arr,(len(arr),1))
+    
+            solution = np.concatenate((value,arr_reshaped), axis=1)
+            np.savetxt(f, solution)
