513cd5b0b68df017ca382718670c19083309d2be
Tadej Vobner <t.vobner@gmail.com>
2021-12-26 13:09:08 +0100

    All minus k

--- npz_to_csv.py
+++ npz_to_csv.py
@@ -1,9 +1,24 @@
 import numpy as np
 import pickle
 
-
+"""
 for i in range(1,17):
     with open("csvData/participant{}".format(i) + ".csv", "a") as f:
+        data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(i))
+        data = pickle.loads(data_npz['data'])
+        labels = pickle.loads(data_npz['label'])
+        for key, value in data.items():
+            # first creating a vector (value) wich cointains all features data
+            # then creating a another vector(arr) wich cointains the labels
+            arr = np.array(labels[key])
+            # reshaping arr into a vertical vector
+            arr_reshaped = np.reshape(arr,(len(arr),1))
+            solution = np.concatenate((value,arr_reshaped), axis=1)
+            np.savetxt(f, solution)
+"""
+
+with open("csvData/participant_all" + ".csv", "a") as f:
+    for i in range(1,17):
         data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(i))
         data = pickle.loads(data_npz['data'])
         labels = pickle.loads(data_npz['label'])
@@ -16,3 +31,32 @@ for i in range(1,17):
             # merging the two vectors together
             solution = np.concatenate((value,arr_reshaped), axis=1)
             np.savetxt(f, solution)
+
+for b in range(1,17):
+    with open("csvData/participant_all-{}".format(b) + ".csv", "a") as f:
+        for i in range(1,b):
+            data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(i))
+            data = pickle.loads(data_npz['data'])
+            labels = pickle.loads(data_npz['label'])
+            for key, value in data.items():
+                # first creating a vector (value) wich cointains all features data
+                # then creating a another vector(arr) wich cointains the labels
+                arr = np.array(labels[key])
+                # reshaping arr into a vertical vector
+                arr_reshaped = np.reshape(arr,(len(arr),1))
+                # merging the two vectors together
+                solution = np.concatenate((value,arr_reshaped), axis=1)
+                np.savetxt(f, solution)
+        for i in range(b+1,17):
+            data_npz = np.load("Data/EEG_DE_features/{}_123.npz".format(i))
+            data = pickle.loads(data_npz['data'])
+            labels = pickle.loads(data_npz['label'])
+            for key, value in data.items():
+                # first creating a vector (value) wich cointains all features data
+                # then creating a another vector(arr) wich cointains the labels
+                arr = np.array(labels[key])
+                # reshaping arr into a vertical vector
+                arr_reshaped = np.reshape(arr,(len(arr),1))
+                # merging the two vectors together
+                solution = np.concatenate((value,arr_reshaped), axis=1)
+                np.savetxt(f, solution)
